e3dd3d45a76cad442bf34aadf6e3d0389a5659e7
ac2393921 <ac2393921@gmail.com>
2022-07-16 01:10:03 +0900

    fix: fix workflow

---
+++ .github/push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git add -N .
+set +e
+git diff --name-only --exit-code
+
+if [[ $? -eq 1 ]]; then
+    echo 'There are changes'
+    git commit -m "add: solver"
+    git push origin main
+else
+    echo 'There are no changes'
+fi
