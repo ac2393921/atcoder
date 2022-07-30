4f290547d8e966199ccafff9ccef217b061b9269
ac2393921 <ac2393921@gmail.com>
2022-07-30 22:01:03 +0900

    add: solve abc040

---
+++ abc/040/a/solver.py
@@ -0,0 +1,3 @@
+n, x = map(int, input().split())
+print(min(abs(n - x), abs(1 - x)))
+
--- abc/042/a/solver.py
+++ abc/042/a/solver.py
@@ -1,3 +1,12 @@
-s = input()
-i = int(input())
-print(s[i - 1])
+from collections import defaultdict
+
+d = defaultdict(int)
+A, B, C = map(int, input().split())
+d[A] += 1
+d[B] += 1
+d[C] += 1
+
+if d[5] == 2 and d[7] == 1:
+    print("Yes")
+else:
+    print("No")
