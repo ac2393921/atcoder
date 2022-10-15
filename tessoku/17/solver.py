323cf86feff5bd011cb829bf698aeb0262ab36c0
ac2393921 <ac2393921@gmail.com>
2022-10-16 00:14:19 +0900

    add: tessoku 17 solver

---
+++ tessoku/17/solver.py
@@ -0,0 +1,59 @@
+import sys
+import re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+
+
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+
+def main():
+    N = i_input()
+    A = i_list()
+    B = i_list()
+    d = [0] * N
+    d[0] = 0
+    d[1] = A[0]
+
+    for i in range(2, N):
+        d[i] = min(d[i-1]+A[i-1], d[i-2]+B[i-2])
+
+    p = N-1
+    ans = []
+    while True:
+        ans.append(p+1)
+        if p == 0:
+            break
+
+        if d[p-1] + A[p-1] == d[p]:
+            p -= 1
+        else:
+            p -= 2
+
+    print(len(ans))
+    print(*sorted(ans))
+
+
+if __name__ == "__main__":
+    main()
