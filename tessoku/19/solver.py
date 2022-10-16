e47bdb19adce4aee2e45627b3f13986c83d009d6
ac2393921 <ac2393921@gmail.com>
2022-10-17 00:51:44 +0900

    add: tessoku 19 solver

---
+++ tessoku/19/solver.py
@@ -0,0 +1,47 @@
+import sys
+import re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+
+
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+
+def main():
+    N, W = i_map()
+    dp = [[0] * (W+1) for _ in range(N+1)]
+
+    for i in range(N):
+        w, v = i_map()
+        for j in range(W+1):
+            if j < w:
+                dp[i+1][j] = dp[i][j]
+            else:
+                dp[i+1][j] = max(dp[i][j], dp[i][j-w] + v)
+
+    print(dp[N][W])
+
+
+if __name__ == "__main__":
+    main()
