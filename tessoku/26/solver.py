76c1582903dbfcacf98961050a09e7bc18df69fa
ac2393921 <ac2393921@gmail.com>
2022-10-21 00:40:14 +0900

    add: tessoku 26 solver

---
+++ tessoku/26/solver.py
@@ -0,0 +1,50 @@
+import math
+import sys
+import re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline
+def ii(): return int(input())
+def im(): return map(int, input().split())
+def il(): return list(im())
+def ir(N): return [ii() for _ in range(N)]
+def irl(N): return [il() for _ in range(N)]
+def si(): return input()
+def sm(): return input().split()
+def sl(): return list(sm())
+def sr(N): return [si for _ in range(N)]
+def srs(N): return [sl() for _ in range(N)]
+def srl(N): return [list(si()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+
+
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+
+def is_prime(x):
+    if x == 2:
+        return False
+
+    for i in range(2, math.ceil(x**0.5)+1):
+        if x % i == 0:
+            return True
+    return False
+
+
+def main():
+    Q = ii()
+    for _ in range(Q):
+        X = ii()
+        print("No" if is_prime(X) else "Yes")
+
+
+if __name__ == "__main__":
+    main()
