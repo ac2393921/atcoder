a7210039e5360f9d86accaea3bb169792d2ad90f
ac2393921 <ac2393921@gmail.com>
2022-10-31 21:37:00 +0900

    update: 40 solver

---
+++ tessoku/40/solver.py
@@ -0,0 +1,47 @@
+from operator import mul
+import sys
+import re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline
+def ii(): return int(input())
+def im(): return map(int, input().split())
+def il(): return list(im())
+def ir(N): return [ii() for _ in range(N)]
+def irl(N): return [il() for _ in range(N)]
+def si(): return input()
+def sm(): return input().split()
+def sl(): return list(sm())
+def sr(N): return [si for _ in range(N)]
+def srs(N): return [sl() for _ in range(N)]
+def srl(N): return [list(si()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+
+
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+
+def main():
+    N = ii()
+    A = il()
+    d = defaultdict(int)
+    for i in range(N):
+        d[A[i]] += 1
+
+    ans = 0
+    for n in d.values():
+        ans += n * (n-1) * (n-2) // 6
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
