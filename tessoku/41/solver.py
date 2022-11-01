47ee2723f492ee57bebf37343b47763217ac4ad3
ac2393921 <ac2393921@gmail.com>
2022-11-01 22:10:10 +0900

    update: 41 solver

---
+++ tessoku/41/solver.py
@@ -0,0 +1,49 @@
+import sys
+import re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline
+def ii(): return int(input())
+def im(): return map(int, input().split())
+def il(): return list(im())
+def ir(N): return [ii() for _ in range(N)]
+def irl(N): return [il() for _ in range(N)]
+def si(): return input()
+def sm(): return input().split()
+def sl(): return list(sm())
+def sr(N): return [si for _ in range(N)]
+def srs(N): return [sl() for _ in range(N)]
+def srl(N): return [list(si()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+
+
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+
+def main():
+    N = ii()
+    S = si()
+    N += 1
+    S = ""+S
+
+    for i in range(N-3):
+        if S[i] == "R" and S[i+1] == "R" and S[i+2] == "R":
+            print("Yes")
+            exit()
+        if S[i] == "B" and S[i+1] == "B" and S[i+2] == "B":
+            print("Yes")
+            exit()
+
+    print("No")
+
+
+if __name__ == "__main__":
+    main()
