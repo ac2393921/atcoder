74fb4790ed148f5c58f8f8c8b17679a2b15b5d80
ac2393921 <ac2393921@gmail.com>
2022-11-05 00:28:31 +0900

    update: abc 273 B solver

---
+++ abc/273/B/solver2.py
@@ -0,0 +1,46 @@
+import sys
+import re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline
+def ii(): return int(input())
+def im(): return map(int, input().split())
+def il(): return list(im())
+def ir(N): return [ii() for _ in range(N)]
+def irl(N): return [il() for _ in range(N)]
+def si(): return input()
+def sm(): return input().split()
+def sl(): return list(sm())
+def sr(N): return [si for _ in range(N)]
+def srs(N): return [sl() for _ in range(N)]
+def srl(N): return [list(si()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+
+
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+
+def main():
+    X, K = im()
+    t = 1
+    for i in range(K):
+        # 3141592の10**iの位を取得（切り上げ判定用）
+        d = X // t % 10
+        t *= 10
+        # 10**i以下を0にする
+        X = X // t*t
+        if d >= 5:
+            X += t
+    print(X)
+
+
+if __name__ == "__main__":
+    main()
