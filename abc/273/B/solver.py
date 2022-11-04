15cf1fb9643aedfb7670c4268b9ecc2e9f045f5c
ac2393921 <ac2393921@gmail.com>
2022-11-05 00:01:39 +0900

    update: abc 273 B solver

---
+++ abc/273/B/solver.py
@@ -0,0 +1,49 @@
+import decimal
+import math
+import sys
+import re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline
+def ii(): return int(input())
+def im(): return map(int, input().split())
+def il(): return list(im())
+def ir(N): return [ii() for _ in range(N)]
+def irl(N): return [il() for _ in range(N)]
+def si(): return input()
+def sm(): return input().split()
+def sl(): return list(sm())
+def sr(N): return [si for _ in range(N)]
+def srs(N): return [sl() for _ in range(N)]
+def srl(N): return [list(si()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+
+
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+
+def main():
+    X, K = im()
+    len_X = len(str(X))
+    if len_X < K:
+        print(0)
+        exit()
+
+    for i in range(K+1):
+        if len_X < i:
+            print(0)
+        X = int(Decimal(X).quantize(
+            Decimal('1E' + str(i)), rounding=decimal.ROUND_HALF_UP))
+    print(X)
+
+
+if __name__ == "__main__":
+    main()
