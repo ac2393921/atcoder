6dace2d3621ef05f84526b04886a37c63828223c
ac2393921 <ac2393921@gmail.com>
2022-11-06 23:34:16 +0900

    update: tessoku 52 solver

---
+++ tessoku/52/solver.py
@@ -0,0 +1,46 @@
+import sys
+import re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline
+def ii(): return int(input())
+def im(): return map(int, input().split())
+def il(): return list(im())
+def ir(N): return [ii() for _ in range(N)]
+def irl(N): return [il() for _ in range(N)]
+def si(): return input()
+def sm(): return input().split()
+def sl(): return list(sm())
+def sr(N): return [si for _ in range(N)]
+def srs(N): return [sl() for _ in range(N)]
+def srl(N): return [list(si()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+
+
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+
+def main():
+    Q = ii()
+
+    queue = deque()
+    for i in range(Q):
+        q = sl()
+        if q[0] == "1":
+            queue.append(q[1])
+        elif q[0] == "2":
+            print(queue[0])
+        else:
+            queue.popleft()
+
+
+if __name__ == "__main__":
+    main()
