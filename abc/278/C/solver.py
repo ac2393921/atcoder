4ad67919e1a18a3e0595b810460b3616e749a793
ac2393921 <ac2393921@gmail.com>
2022-11-23 22:37:49 +0900

    update: abc 278 C solver

---
+++ abc/278/C/solver.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+import sys
+import re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline
+def ii(): return int(input())
+def im(): return map(int, input().split())
+def il(): return list(im())
+def ir(N): return [ii() for _ in range(N)]
+def irl(N): return [il() for _ in range(N)]
+def si(): return input()
+def sm(): return input().split()
+def sl(): return list(sm())
+def sr(N): return [si for _ in range(N)]
+def srs(N): return [sl() for _ in range(N)]
+def srl(N): return [list(si()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+
+
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+
+def main():
+    N, Q = im()
+    ff = defaultdict(set)
+
+    for i in range(Q):
+        t, a, b = il()
+        if t == 1:
+            ff[a].add(b)
+        if t == 2:
+            if b in ff[a]:
+                ff[a].remove(b)
+        if t == 3:
+            if b in ff[a] and a in ff[b]:
+                print("Yes")
+            else:
+                print("No")
+
+
+if __name__ == "__main__":
+    main()
+
