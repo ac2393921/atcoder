7f102397c24b1be5b661d63cf316861873b163d9
ac2393921 <ac2393921@gmail.com>
2022-11-30 00:26:58 +0900

    add: abc 269 B solver

---
+++ abc/269/B/solver.py
@@ -0,0 +1,48 @@
+import sys
+import re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline
+def ii(): return int(input())
+def im(): return map(int, input().split())
+def il(): return list(im())
+def ir(N): return [ii() for _ in range(N)]
+def irl(N): return [il() for _ in range(N)]
+def si(): return input()
+def sm(): return input().split()
+def sl(): return list(sm())
+def sr(N): return [si for _ in range(N)]
+def srs(N): return [sl() for _ in range(N)]
+def srl(N): return [list(si()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+
+
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+
+def main():
+    S = srl(10)
+    A, B, C, D = INF, 0, INF, 0
+
+    for i in range(10):
+        for j in range(10):
+            if S[i][j] == "#":
+                A = min(A, i+1)
+                B = max(B, i+1)
+                C = min(C, j+1)
+                D = max(D, j+1)
+
+    print(A, B)
+    print(C, D)
+
+
+if __name__ == "__main__":
+    main()
