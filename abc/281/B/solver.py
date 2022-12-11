8dd5abc51fc9fab761c9ca89e05bec233a0aabc7
ac2393921 <ac2393921@gmail.com>
2022-12-12 00:08:03 +0900

    add: abc 281 B solver

---
+++ abc/281/B/solver.py
@@ -0,0 +1,59 @@
+import sys
+import re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline
+def ii(): return int(input())
+def im(): return map(int, input().split())
+def il(): return list(im())
+def ir(N): return [ii() for _ in range(N)]
+def irl(N): return [il() for _ in range(N)]
+def si(): return input()
+def sm(): return input().split()
+def sl(): return list(sm())
+def sr(N): return [si for _ in range(N)]
+def srs(N): return [sl() for _ in range(N)]
+def srl(N): return [list(si()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+
+
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+
+def main():
+    S = si()
+    if len(S) != 8:
+        print("No")
+        exit()
+
+    cnt = 0
+    for i in range(len(S)):
+        if i == 0 and S[i].isdigit():
+            print("No")
+            exit()
+        elif i == len(S)-1 and S[i].isdigit():
+            print("No")
+            exit()
+        elif 0 < i < len(S)-1 and not S[i].isdigit():
+            print("No")
+            exit()
+        elif S[i].isdigit():
+            cnt += 1
+
+    if cnt < 6 < cnt or int(S[1:len(S)-1]) < 100000 or 999999 < int(S[1:len(S)-1]):
+        print("No")
+    else:
+        print("Yes")
+
+
+if __name__ == "__main__":
+    main()
+
